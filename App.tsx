472824c6962852bc188a8108fafd139966aef3d1
Fábio Ceolin <fabioceolin98@gmail.com>
2021-07-03 22:15:41 -0300

    refactor layout

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { StatusBar } from "react-native";
-import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
+import { StatusBar, LogBox } from "react-native";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import {
   Rajdhani_500Medium,
   Rajdhani_700Bold,
 } from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+
+LogBox.ignoreLogs([
+  "You are not currently signed in to Expo on your development machine.",
+]);
 
 import { AuthProvider } from "./src/hooks/auth";
 
+import { Routes } from "./src/routes";
 import { Background } from "./src/components/Background";
 
-import { Routes } from "./src/routes";
-
 export default function App() {
-  let [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
     Rajdhani_500Medium,
@@ -25,6 +28,7 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
+
   return (
     <Background>
       <StatusBar
--- src/configs/discordAuth.ts
+++
@@ -1,8 +0,0 @@
-const REDIRECT_URI =
-  "https://auth.expo.io/@anonymous/gameplay-4628d540-ee17-409c-8265-12ebf28e6ad8";
-const SCOPE = "email%20identify%20connections%20guilds";
-const RESPONSE_TYPE = "token";
-const CLIENT_ID = "859572018609520640";
-const CDN_IMAGE = "https://cdn.discordapp.com";
-
-export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
